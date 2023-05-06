581e3ce3abf11a74c4b6290b30f655779d7246f0
Philipp Heuer <git@philippheuer.me>
2023-05-06 20:27:44 +0200

    chore: add hello world http endpoint  fb655f

--- app.go
+++ app.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello world")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello, World!")
+	})
+
+	err := http.ListenAndServe("0.0.0.0:8000", nil)
+	if err != nil {
+		panic(err)
+	}
 }
